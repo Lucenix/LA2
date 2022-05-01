28b07b1fe80c0fb00633995553d469246622d55e
Lucenix <andrelucenaferreira@gmail.com>
2022-05-01 16:26:02 +0100

    la2 better

--- treino3/vendedor.py
+++ treino3/vendedor.py
@@ -1,14 +1,13 @@
 #def dynamic 100%
 def vendedor(capacidade,produtos):
     ht = {0:[]}
-    mx = {0:0}
+    mxs = {0:0}
     for c in range(1, capacidade+1):
         ht[c] = []
-        mx[c] = 0
+        mxs[c] = 0
         for n,v,p in produtos:
-            if c-p in ht:
-                if v+mx[c-p] > mx[c]:
-                    mx[c] = v+mx[c-p]
-                    ht[c] = ht[c-p].copy()
-                    ht[c].append(n)
-    return (mx[capacidade],sorted(ht[capacidade]))
+            if c-p>=0 and v+mxs[c-p] > mxs[c]:
+                mxs[c] = v+mxs[c-p]
+                ht[c] = ht[c-p].copy()
+                ht[c].append(n)
+    return (mxs[capacidade],sorted(ht[capacidade]))
